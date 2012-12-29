6b45bcbc49a183dafd7105ef164ec8f135e2c99e
Patrick S. Vogt <patrick.vogt.pv@gmail.com>
2012-12-29 14:45:07 +0100

    cleanup

--- src/ch/almana/android/sessionsmodels/view/models/SessionsFragment.java
+++ src/ch/almana/android/sessionsmodels/view/models/SessionsFragment.java
@@ -1,7 +1,6 @@
 package ch.almana.android.sessionsmodels.view.models;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,46 +10,20 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
-import android.widget.Gallery;
-import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.SpinnerAdapter;
 import ch.almana.android.sessionsmodels.R;
-import ch.almana.android.sessionsmodels.helper.ImageHelper;
-import ch.almana.android.sessionsmodels.log.Logger;
 import ch.almana.android.sessionsmodels.model.SessionModel;
 import ch.almana.android.sessionsmodels.view.ImagePagerActivity;
-import ch.almana.android.sessionsmodels.view.ModelDetailActivity;
-import ch.almana.android.sessionsmodels.view.ModelListActivity;
 import ch.almana.android.sessionsmodels.view.ModelListFragment;
 
-/**
- * A fragment representing a single Model detail screen. This fragment is either
- * contained in a {@link ModelListActivity} in two-pane mode (on tablets) or a
- * {@link ModelDetailActivity} on handsets.
- */
 public class SessionsFragment extends Fragment implements OnItemClickListener {
 
-	private static boolean useGallery = false;
-
-	/**
-	 * The dummy content this fragment is presenting.
-	 */
-	private SessionModel mItem;
-
-	//	private LinearLayout llTop;
-
-	@SuppressWarnings("deprecation")
-	private Gallery gallery;
+	private SessionModel session;
 
 	private ListView listview;
 
 	private int sessionId;
 
-	/**
-	 * Mandatory empty constructor for the fragment manager to instantiate the
-	 * fragment (e.g. upon screen orientation changes).
-	 */
 	public SessionsFragment() {
 	}
 
@@ -60,21 +33,16 @@ public class SessionsFragment extends Fragment implements OnItemClickListener {
 
 		if (getArguments().containsKey(ModelDetailFragment.EXTRA_MODEL_ID)) {
 			sessionId = getArguments().getInt(ModelDetailFragment.EXTRA_MODEL_ID);
-			mItem = (SessionModel) ModelListFragment.getListItems(getActivity()).get(sessionId);
+			session = (SessionModel) ModelListFragment.getListItems(getActivity()).get(sessionId);
 		}
 	}
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		View rootView = inflater.inflate(useGallery ? R.layout.fragment_sessions_gallery : R.layout.fragment_sessions, container, false);
-		if (useGallery) {
-			gallery = ((Gallery) rootView.findViewById(R.id.gallery1));
-		} else {
-			//			llTop = ((LinearLayout) rootView.findViewById(R.id.llTop));
-			listview = ((ListView) rootView.findViewById(android.R.id.list));
-			listview.setOnItemClickListener(this);
-		}
+		View rootView = inflater.inflate(R.layout.fragment_sessions, container, false);
+		listview = ((ListView) rootView.findViewById(android.R.id.list));
+		listview.setOnItemClickListener(this);
 		return rootView;
 	}
 
@@ -82,34 +50,11 @@ public class SessionsFragment extends Fragment implements OnItemClickListener {
 	public void onResume() {
 		super.onResume();
 
-		// Show the dummy content as text in a TextView.
-		if (mItem != null) {
-			File[] images = mItem.getDir().listFiles();
-			if (useGallery) {
-				SpinnerAdapter adapter = new ImageAdapter(images, 250);
-				gallery.setAdapter(adapter);
-			} else {
-				//				llTop.removeAllViews();
-				//				for (int i = 0; i < images.length; i++) {
-				//					llTop.addView(getImage(images[i]));
-				//				}
-				listview.setAdapter(new ImageAdapter(images, 250));
-			}
+		if (session != null) {
+			File[] images = session.getDir().listFiles();
+			listview.setAdapter(new ImageAdapter(images, 250));
 
 		}
-	}
-
-	private ImageView getImage(File image) {
-		ImageView iv = new ImageView(getActivity());
-
-		try {
-			iv.setImageBitmap(ImageHelper.scaleImage(getActivity(), image, 250));
-		} catch (FileNotFoundException e) {
-			Logger.w("Cannot load image", e);
-		}
-		//			ImageView ivModelImage = (ImageView) rootView.findViewById(R.id.ivModelImage);
-		//			ivModelImage.setImageURI(Uri.fromFile(mItem.image));
-		return iv;
 	}
 
 	@Override
